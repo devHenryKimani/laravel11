85a1cfac58b71456be9a162b8346e183a305160a
devHenrykimani <kimhenry756@gmail.com>
2025-08-25 15:55:38 +0300

    laravel

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Index</div>
+    <MainLayout>
+    <Link href="/hello">ShowPage</Link>
+     <div>
+        The message is {{ message }}
+     </div>
+    </MainLayout>
+</template>      
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import MainLayout from '../../Layouts/MainLayout.vue';
+defineProps({
+    message: String
+});
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <MainLayout>
+    <div>Show</div>
+    <Link href="/hello">mainPage</Link>
+    <div>{{ message }}</div>
+    </MainLayout>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import MainLayout from '../../Layouts/MainLayout.vue';
+defineProps({
+    message:String
+});
+</script>
