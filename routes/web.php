c6c07a7a72fac83c9c731bf8b95841360feb91d9
devHenrykimani <kimhenry756@gmail.com>
2025-09-10 10:04:33 +0300

    migrations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
  
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello',[IndexController::class, 'show']);
+
