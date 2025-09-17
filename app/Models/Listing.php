add3bab733e7ccf546609e2955b6f6fdc7ace161
devHenrykimani <kimhenry756@gmail.com>
2025-09-17 16:11:17 +0300

    listingfactory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Listing extends Model
 {
-    //
+    
+
+
+    use HasFactory;
+       protected $fillable = [
+
+
+
+        'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'
+
+
+    ];
+
 }
