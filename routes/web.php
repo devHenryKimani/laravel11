2aabba1159572e84e2bc84a60dd22ac4c69fa9ac
devHenrykimani <kimhenry756@gmail.com>
2025-09-17 18:27:35 +0300

    listingReasource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
-
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +11,4 @@ Route::get('/', function () {
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello',[IndexController::class, 'show']);
 
+Route::resource('Listing',ListingController::class)->only(['index', 'show']);
