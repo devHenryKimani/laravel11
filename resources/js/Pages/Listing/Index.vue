2037539be63c949c687b5432b9e8c931e5ee52e4
devHenrykimani <kimhenry756@gmail.com>
2025-09-22 11:42:09 +0300

    ListingAdress

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,12 +1,17 @@
 <template>
-<div v-for="listing in listings" :key="listing.id">   
-    {{ listing.city }} - {{ listing.street }}
-</div>
-   
+
+ <div v-for="listing in listings" :key="listing.id">
+
+    <Link :href="`/listing/${listing.id}`">
+      <ListingAddress :listing="listing" />
+    </Link>
+  </div>
 </template>
 
 
 <script setup>
+import { Link} from '@inertiajs/vue3';
+import ListingAddress from '../../Components/ListingAddress.vue';
 defineProps({
     listings:Array,
 });
