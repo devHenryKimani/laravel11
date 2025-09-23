dd1978509878e8a2164f9c03b5bce0a466292ce5
devHenrykimani <kimhenry756@gmail.com>
2025-09-23 17:20:08 +0300

    handlingForms

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@ Route::get('/', function () {
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello',[IndexController::class, 'show']);
 
-Route::resource('listing',ListingController::class)->only(['index', 'show']);
+Route::resource('listing',ListingController::class)
+
+ ->only(['index', 'show', 'create']);
