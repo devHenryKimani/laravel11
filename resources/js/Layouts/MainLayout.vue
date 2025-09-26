314cd98e123ad83b71a0af85583458f8a38b2477
devHenrykimani <kimhenry756@gmail.com>
2025-09-26 17:24:40 +0300

    computed

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,16 +2,31 @@
 <Link href="/">Main Page</Link>&nbsp;
 <Link href="/hello">Show Page</Link>
 <!-- <div>The page with time{{ timer }}</div> -->
+ <div v-if="flashSuccess" class="success">
+  {{ flashSuccess }}
+ </div>
+ 
 <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
+import { Link , usePage} from '@inertiajs/vue3'
+import { computed } from 'vue';
 
 // const timer = ref(0)
 
 // setInterval(() => {
 //   timer.value++
 // }, 1000)
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+)
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+</style>
