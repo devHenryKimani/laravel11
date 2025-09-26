34542270337242f75c1f42ee4dc152e3e0c8c2a9
devHenrykimani <kimhenry756@gmail.com>
2025-09-26 11:34:33 +0300

    sucessMessage

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -34,7 +34,8 @@ class ListingController extends Controller
     public function store(Request $request)
     {
         Listing::create($request->all());
-        return redirect()->route('listing.index');
+        return redirect()->route('listing.index')
+        >with('success', 'Listing was created!');
         
     }
 
