f343ff69abab0e9b8ffbbc331bc588d11eb86239
devHenrykimani <kimhenry756@gmail.com>
2025-10-01 19:05:49 +0300

    EditForm

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,9 +2,14 @@
 
  <div v-for="listing in listings" :key="listing.id">
 
-    <Link :href="`/listing/${listing.id}`">
+    <div>
+      <Link :href="`/listing/${listing.id}`">
       <ListingAddress :listing="listing" />
     </Link>
+    </div>
+    <div>
+      <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
+    </div>
   </div>
 </template>
 
