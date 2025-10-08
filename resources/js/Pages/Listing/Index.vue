95ccd44e13f75768428e68097f29a5adcbb43582
devHenrykimani <kimhenry756@gmail.com>
2025-10-08 16:13:17 +0300

    flex

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -5,6 +5,7 @@
 
     <div>
       <Link :href="route('listing.show', {listing: listing.id})">
+        <Price :price="listing.price" class="text-2xl font-bold" />
          <ListingSpace :listing="listing" class="text-lg" />
       <ListingAddress :listing="listing" class="text-gray-500" />
     </Link>
@@ -27,6 +28,7 @@ import MainLayout from '../../Layouts/MainLayout.vue';
 import ListingAddress from '../../Components/ListingAddress.vue';
 import Box from '../../Components/UI/Box.vue';
 import ListingSpace from '../../Components/ListingSpace.vue';
+import Price from '../../Components/Price.vue';
 defineProps({
     listings:Array,
 });
