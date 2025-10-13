a5115afb309f3ed304d431f1c6702bc66f8cb98d
devHenrykimani <kimhenry756@gmail.com>
2025-10-13 20:23:43 +0300

    login markup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
- public function create() {
+ public function create() 
+ {
+  return inertia('Auth/Login');
+
+  
+ }
+ public function store() 
+ {
 
  }
- public function store() {
 
- }
-
- public function destroy() {
+ public function destroy() 
+ {
     
  }
 }
